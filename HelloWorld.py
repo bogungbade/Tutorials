f3eff6456cdac13e1cc7340edf7692df4a30ca09
bogungbade <bayuzene@yahoo.com>
2019-11-21 13:28:40 -0600

    Initial commit: Basic Python Tutorials

---
+++ HelloWorld.py
@@ -0,0 +1,39 @@
+# print ("Hello, World!")
+# print(33+24)
+# print(70*3)
+# print()
+# print ('The End')
+# print()
+# print("I wonder if you can use 'strings' in quotes")
+# print('I wonder if you can use "quotes" in strings')
+# print("Hello" + " " + "World")
+
+# To put a space between words, you can insert ' ' or " "
+# A = "Hello"
+# B = "Bayo"
+# print(A + ' ' + B)
+
+# C = "Hello"
+# D = input("Please enter your name")
+# print(C + ' ' + D)
+
+# splitString = "This string has been\n split \nover\nseveral lines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t6"
+# print(tabbedString)
+# print()
+# print("""The quick brown fox
+# jumped over
+# the lazy dog""");
+
+# print('The pet shop owner said "No, No, \'e\'s uh,...he\'s resting"')
+# print('''The pet shop owner said "No, No, 'e's uh,...he's resting"''')
+# print("""The pet shop owner said "No, No, 'e's uh,...he's resting" """)
+
+x = "You were born in "
+y = input("Please input your age ")
+z = 2019 - (y)
+print(x + z)
+
+
